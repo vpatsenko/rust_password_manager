a0ec9a9f961930bd4a742395fe6cd8f25cf88430
Vladimir Patsenko <v@patsenko.ru>
2022-06-21 07:35:36 +0300

    encryption

--- src/main.rs
+++ src/main.rs
@@ -22,39 +22,80 @@
 //     // }
 // }
 
-use crypto::aead::{AeadDecryptor, AeadEncryptor};
-use crypto::aes_gcm::AesGcm;
-use rand::Rng;
-use rustc_serialize::hex::FromHex;
+// use crypto::aead::{AeadDecryptor, AeadEncryptor};
+// use crypto::aes_gcm::AesGcm;
+// use rand::Rng;
+// use rustc_serialize::hex::FromHex;
 
-use core::str;
-use crypto::aes::{self, KeySize};
-use crypto::digest::Digest;
-use crypto::hmac::Hmac;
-use crypto::mac::Mac;
-use crypto::sha2::Sha256;
-use crypto::symmetriccipher::SynchronousStreamCipher;
+// use core::str;
+// use crypto::aes::{self, KeySize};
+// use crypto::digest::Digest;
+// use crypto::hmac::Hmac;
+// use crypto::mac::Mac;
+// use crypto::sha2::Sha256;
+// use crypto::symmetriccipher::SynchronousStreamCipher;
 
-use rustc_serialize::base64::{ToBase64, STANDARD};
-use rustc_serialize::hex::ToHex;
+// use rustc_serialize::base64::{ToBase64, STANDARD};
+// use rustc_serialize::hex::ToHex;
 
-use std::iter::repeat;
+// use std::iter::repeat;
 
-use rand;
-use std::env;
+// use rand;
+// use std::env;
 
 fn main() {
-    let mut gen = rand::thread_rng().gen_range(0..10);
-    rand::rngs::OsRng::new()
-    let mut key: Vec<u8> = repeat(0u8).take(16).collect();
-    gen.fill_bytes(&mut key[..]);
-    let mut nonce: Vec<u8> = repeat(0u8).take(16).collect();
-    gen.fill_bytes(&mut nonce[..]);
-    println!("Key: {}", key.to_base64(STANDARD));
-    println!("Nonce: {}", nonce.to_base64(STANDARD));
-    let mut cipher = aes::ctr(KeySize::KeySize128, &key, &nonce);
-    let secret = "I like Nickelback";
-    let mut output: Vec<u8> = repeat(0u8).take(secret.len()).collect();
-    cipher.process(secret.as_bytes(), &mut output[..]);
-    println!("Ciphertext: {}", output.to_base64(STANDARD));
+    // let mut gen = rand::thread_rng().gen_range(0..10);
+    // rand::rngs::OsRng::
+    // let mut key: Vec<u8> = repeat(0u8).take(16).collect();
+    // gen.fill_bytes(&mut key[..]);
+    // let mut nonce: Vec<u8> = repeat(0u8).take(16).collect();
+    // gen.fill_bytes(&mut nonce[..]);
+    // println!("Key: {}", key.to_base64(STANDARD));
+    // println!("Nonce: {}", nonce.to_base64(STANDARD));
+    // let mut cipher = aes::ctr(KeySize::KeySize128, &key, &nonce);
+    // let secret = "I like Nickelback";
+    // let mut output: Vec<u8> = repeat(0u8).take(secret.len()).collect();
+    // cipher.process(secret.as_bytes(), &mut output[..]);
+    // println!("Ciphertext: {}", output.to_base64(STANDARD));
+
+    use aes::cipher::{
+        generic_array::GenericArray, BlockCipher, BlockDecrypt, BlockEncrypt, KeyInit,
+    };
+    use aes::Aes128;
+
+    let key = GenericArray::from([0u8; 16]);
+    println!("key: {:?}", key);
+    let mut block = GenericArray::from([42u8; 16]);
+
+    // Initialize cipher
+    let cipher = Aes128::new(&key);
+
+    let block_copy = block.clone();
+
+    // Encrypt block in-place
+    cipher.encrypt_block(&mut block);
+    println!("encrypted block: {:?}", block);
+
+    // And decrypt it back
+    cipher.decrypt_block(&mut block);
+    println!("decrypted block: {:?}", block);
+    assert_eq!(block, block_copy);
+
+    // implementation supports parrallel block processing
+    // number of blocks processed in parallel depends in general
+    // on hardware capabilities
+    let mut blocks = [block; 100];
+    cipher.encrypt_blocks(&mut blocks);
+
+    for block in blocks.iter_mut() {
+        cipher.decrypt_block(block);
+        assert_eq!(block, &block_copy);
+    }
+
+    cipher.decrypt_blocks(&mut blocks);
+
+    for block in blocks.iter_mut() {
+        cipher.encrypt_block(block);
+        assert_eq!(block, &block_copy);
+    }
 }
